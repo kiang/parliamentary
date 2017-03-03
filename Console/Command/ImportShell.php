9d402840baa11424f65dbd63a79c484fcf98fef2
kiang <kiange@gmail.com>
2017-03-04 03:42:13 +0800

    to import tnccp2

--- Console/Command/ImportShell.php
+++ Console/Command/ImportShell.php
@@ -5,9 +5,9 @@ class ImportShell extends AppShell {
     public $uses = array('Parliamentarian');
 
     public function main() {
-        $jsonFile = TMP . 'tnccp.json';
+        $jsonFile = TMP . 'tnccp2.json';
         if (!file_exists($jsonFile)) {
-            file_put_contents($jsonFile, file_get_contents('https://github.com/kiang/tncc/raw/master/tnccp.json'));
+            file_put_contents($jsonFile, file_get_contents('https://github.com/kiang/tncc/raw/master/tnccp2/tnccp2.json'));
         }
         $c = json_decode(file_get_contents($jsonFile), true);
         $parties = $this->Parliamentarian->Party->find('list', array(
@@ -15,40 +15,46 @@ class ImportShell extends AppShell {
         ));
         $parliamentarians = array();
         foreach ($c AS $p) {
-            if (!isset($parties[$p['party']])) {
+            $contact = array();
+            foreach ($p['each_terms'][0]['contact_details'] AS $line) {
+                $contact[$line['label']] = $line['value'];
+            }
+            if (!isset($parties[$p['each_terms'][0]['party']])) {
                 $this->Parliamentarian->Party->create();
                 $this->Parliamentarian->Party->save(array('Party' => array(
-                        'name' => $p['party'],
+                        'name' => $p['each_terms'][0]['party'],
                 )));
-                $parties[$p['party']] = $this->Parliamentarian->Party->getInsertID();
+                $parties[$p['each_terms'][0]['party']] = $this->Parliamentarian->Party->getInsertID();
             }
             $pDb = $this->Parliamentarian->find('first', array(
                 'conditions' => array(
                     'name' => $p['name'],
-                    'district' => $p['district'],
+                    'district' => $p['each_terms'][0]['district'],
+                    'ad' => '2',
                 ),
             ));
             if (empty($pDb)) {
                 $this->Parliamentarian->create();
                 $this->Parliamentarian->save(array('Parliamentarian' => array(
-                        'Party_id' => $parties[$p['party']],
+                        'Party_id' => $parties[$p['each_terms'][0]['party']],
                         'name' => $p['name'],
-                        'district' => $p['district'],
-                        'contacts_phone' => $p['contacts']['phone'],
-                        'contacts_fax' => $p['contacts']['fax'],
-                        'contacts_email' => $p['contacts']['email'],
-                        'contacts_address' => $p['contacts']['address'],
-                        'links_council' => $p['links']['council'],
-                        'gender' => ($p['gender'] === '女性') ? 'F' : 'M',
-                        'image_url' => $p['image'],
-                        'experience' => implode("\n", $p['experience']),
-                        'platform' => (is_array($p['platform'])) ? implode("\n", $p['platform']) : $p['platform'],
+                        'district' => $p['each_terms'][0]['district'],
+                        'contacts_phone' => isset($contact['電話']) ? $contact['電話'] : '',
+                        'contacts_fax' => isset($contact['傳真']) ? $contact['傳真'] : '',
+                        'contacts_email' => isset($contact['電子信箱']) ? $contact['電子信箱'] : '',
+                        'contacts_address' => isset($contact['通訊處']) ? $contact['通訊處'] : '',
+                        'links_council' => $p['each_terms'][0]['links'][0]['url'],
+                        'gender' => ($p['each_terms'][0]['gender'] === '女性') ? 'F' : 'M',
+                        'image_url' => $p['each_terms'][0]['image'],
+                        'experience' => implode("\n", $p['each_terms'][0]['experience']),
+                        'platform' => (is_array($p['each_terms'][0]['platform'])) ? implode("\n", $p['each_terms'][0]['platform']) : $p['each_terms'][0]['platform'],
                         'birth' => date('Y-m-d', strtotime($p['birth'])),
-                        'party' => $p['party'],
-                        'constituency' => $p['constituency'],
-                        'education' => implode("\n", $p['education']),
-                        'group' => $p['group'],
-                        'ad' => $p['ad'],
+                        'party' => $p['each_terms'][0]['party'],
+                        'constituency' => $p['each_terms'][0]['constituency'],
+                        'education' => implode("\n", $p['each_terms'][0]['education']),
+                        'ad' => '2',
+                        'count_submits' => 0,
+                        'count_petitions' => 0,
                 )));
                 $pDb = $this->Parliamentarian->read();
             }
@@ -58,22 +64,26 @@ class ImportShell extends AppShell {
             $parliamentarians[$pDb['Parliamentarian']['district']][$pDb['Parliamentarian']['name']] = $pDb;
         }
         
+        print_r($parliamentarians);
+
+        return;
+
         $jsonMotionFiles = array();
-        
+
         foreach (glob('/home/kiang/public_html/tncc/motions/*/*/*.json') AS $jsonMotionFile) {
             $jsonMotionFiles[] = $jsonMotionFile;
         }
-        
+
         foreach (glob('/home/kiang/public_html/tncc/motions/misc/*.json') AS $jsonMotionFile) {
             $jsonMotionFiles[] = $jsonMotionFile;
         }
-        
+
         foreach ($jsonMotionFiles AS $jsonMotionFile) {
             $jsonMotion = json_decode(file_get_contents($jsonMotionFile), true);
             $motionId = substr($jsonMotionFile, -41, 36);
-            if($this->Parliamentarian->Motion->find('count', array(
-                'conditions' => array('id' => $motionId),
-            )) === 0) {
+            if ($this->Parliamentarian->Motion->find('count', array(
+                        'conditions' => array('id' => $motionId),
+                    )) === 0) {
                 $this->Parliamentarian->Motion->create();
             } else {
                 $this->Parliamentarian->Motion->id = $motionId;
